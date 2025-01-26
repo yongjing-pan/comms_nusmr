72f71260d5d870809dd34ffc77347f9ec4a5e3d8
Mayukh Ghosh <mayukhghosh1975@gmail.com>
2025-01-26 18:43:54 +0800

    Update teststream.py

--- teststream.py
+++ teststream.py
@@ -1,19 +1,28 @@
 import subprocess
-import time
-# Run ffmpeg to capture the video from /dev/video2 and pipe it to ffplay
+import signal
+import sys
+
 ffmpeg_command = [
     'ffmpeg', '-re','-f', 'v4l2', '-framerate', '15', '-video_size', '640x480', '-pixel_format', 'mjpeg', 
     '-i', '/dev/video1','-c:v', 'libx264', '-b:v', '1M', '-preset', 'ultrafast', 
-    '-tune', 'zerolatency','-loglevel','debug', '-f', 'mpegts','-bufsize','32M', '-t','60','-'
+    '-tune', 'zerolatency','-loglevel','debug', '-f', 'mpegts','-flush_packets','0','-bufsize','64M','-'
 ]
-# can remove -re, loglevel debug, 
+#can remove -re, loglevel debug
 process_ffmpeg = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE)
 
 ffplay_command = [
     'ffplay', '-fflags', 'nobuffer', '-flags', 'low_delay', '-i', '-'
 ]
 
-process_ffplay = subprocess.Popen(ffplay_command, stdin=process_ffmpeg.stdout, stderr=subprocess.PIPE)
+process_ffplay = subprocess.Popen(ffplay_command, stdin=process_ffmpeg.stdout)
+
+def signal_handler(sig, frame):
+    print("Interrupt received, stopping stream.")
+    process_ffmpeg.terminate()
+    process_ffplay.terminate()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 process_ffplay.wait()
 
