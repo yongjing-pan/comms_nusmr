186ec08cc12a0d9e38c2f7e41c51d8b53c9eec81
yongjing-pan <yongjing.pan03@gmail.com>
2025-01-30 18:48:14 +0800

    Stream to other device

--- teststream.py
+++ teststream.py
@@ -3,11 +3,25 @@ import signal
 import sys
 
 #infact can just run these instead of everything else. However call() blocks anything else from running so Popen is still better for our purposes.
+#Stream from your device to same device
 
+cam_source = "/dev/video0"
+cam_source2 = "/dev/video2"
+cam_source3 = "/dev/video4"
+
+"""
 command = "ffmpeg -f v4l2 -framerate 15 -video_size 640x480 -pixel_format mjpeg -i /dev/video1 -c:v libx264 -b:v 1.3M -preset ultrafast -tune zerolatency -f mpegts -flush_packets 0 -bufsize 2M -probesize 32M - | ffplay -fflags nobuffer -flags low_delay -i -"
 subprocess.call(command, shell=True)
+"""
 
+stream_to = "192.168.153.100:20001"
+probe_size = "250k"
+buffer_size = "1M"
+flush_packets = "0"
+command2 = "ffmpeg -f v4l2 -framerate 15 -r 50 -video_size 640x480 -pixel_format mjpeg -i "+cam_source+" -c:v libx264 -b:v 1.3M -preset ultrafast -tune zerolatency -f mpegts -flush_packets "+flush_packets+" -bufsize "+buffer_size+" -probesize "+probe_size+" udp://"+stream_to
+subprocess.call(command2, shell=True)
 
+#Use these to change settings dynamically
 """
 ffmpeg_command = [
     'ffmpeg', '-re','-f', 'v4l2', '-framerate', '15', '-video_size', '640x480', '-pixel_format', 'mjpeg', 
