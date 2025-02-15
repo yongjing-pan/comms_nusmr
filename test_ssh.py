92bbdec5ed929b24bf533d8b4d7434e72edd26b3
yongjing-pan <yongjing.pan03@gmail.com>
2025-02-15 21:35:17 +0800

    Upon closing GUI, it kills all ffmpeg processes on orin and opi. Also added window sizing and positions for each stream for ffplay so it looks neater.

--- test_ssh.py
+++ test_ssh.py
@@ -4,6 +4,10 @@ host = '192.168.1.17'
 username = 'orangepi'
 password = 'orangepi'
 
+host = '192.168.1.16'
+username = 'orinnx'
+password = 'pw'
+
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(host, username=username, password=password)
